07f06bca3369d3c11b64427d276f3e0f5ec11f69
Vũ Phương Nam <namvp@wisgroup.io>
2024-10-31 11:59:43 +0700

    chore: define variable for chain and refactor imports

--- config.ts
+++ config.ts
@@ -1,22 +1,24 @@
 import "dotenv/config";
 
 import { createPublicClient, Hex, http } from "viem";
+import { privateKeyToAccount, generatePrivateKey } from "viem/accounts";
 import { entryPoint06Address } from "viem/account-abstraction";
 import { arbitrumSepolia } from "viem/chains";
-import { privateKeyToAccount, generatePrivateKey } from "viem/accounts";
-import { updateEnvFile } from "./utils";
-import { toBicSmartAccount } from "./accounts/toBicSmartAccounts";
+
 import { createSmartAccountClient } from "permissionless";
 import { createPimlicoClient } from "permissionless/clients/pimlico";
 
-const apiKey = process.env.PIMLICO_API_KEY;
+import { updateEnvFile } from "./utils";
+import { toBicSmartAccount } from "./accounts/toBicSmartAccounts";
 
+const apiKey = process.env.PIMLICO_API_KEY;
+const chain = arbitrumSepolia;
 if (!apiKey) {
   throw new Error("PIMLICO_API_KEY is not set");
 }
 
 const client = createPublicClient({
-  chain: arbitrumSepolia,
+  chain,
   transport: http(),
 });
 
@@ -28,7 +30,7 @@ const privateKey =
     return pk;
   })();
 
-const pimlicoUrl = `https://api.pimlico.io/v2/${arbitrumSepolia.id}/rpc?apikey=${apiKey}`;
+const pimlicoUrl = `https://api.pimlico.io/v2/${chain.id}/rpc?apikey=${apiKey}`;
 
 const pimlicoClient = createPimlicoClient({
   transport: http(pimlicoUrl),
@@ -47,11 +49,15 @@ const account = await toBicSmartAccount({
     version: "0.6",
   },
 });
-console.log(`Account address: ${await account.getAddress()}`);
+console.log(
+  `Account address: ${
+    chain.blockExplorers.default.url
+  }/address/${await account.getAddress()}`
+);
 
 export const smartAccountClient = createSmartAccountClient({
   account,
-  chain: arbitrumSepolia,
+  chain,
   bundlerTransport: http(pimlicoUrl),
   paymaster: pimlicoClient,
   userOperation: {
